072448a3d0e2ff2d9ed5b77713de18790fd29b3f
Duane Wellington <dwelling77@gmail.com>
2023-12-06 14:44:01 -0500

    lesson complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,36 @@
-import "./styles.css";
+// PATH: './App.jsx'
+
+import React, { useState } from "react";
+import "./App.css";
 
 export default function App() {
+  const [formData, setFormData] = useState({
+    name: "",
+    emotion: "😁",
+  });
+
+  function handleChange(evt) {
+    const newFormData = { ...formData, [evt.target.name]: evt.target.value }
+    setFormData(newFormData);
+  }
+
   return (
     <div className="App">
       <form>
-        <label>NAME</label>
-        <input name="name" />
-        <label>EMOTION</label>
-        <select name="emotion">
+        <label>
+          NAME
+          <input name='name' value={formData.name} onChange={handleChange} />
+          </label>
+        <label>
+          EMOTION
+        <select name="emotion" value={formData.emotion} onChange={handleChange}>
           <option value="😁">Happy</option>
           <option value="😐">Neutral</option>
           <option value="😠">Angry</option>
         </select>
+        </label>
       </form>
+      <h1>{formData.name} is {formData.emotion}</h1>
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,6 @@
+// PATH: './main.jsx'
+
+
 // Refactor: removed React import after removing strict mode;
 // strictmode is helpful in production, but can produce extra log messages in development.
 
